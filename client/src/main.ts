6863f6bd834e8c3356fe578c96f4e5fe2ecc6232
Scott Hitchcock <scotthitch5@gmail.com>
2024-01-30 20:30:53 +1300

    add axios base route

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,6 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:8000/";
 
 const app = createApp(App)
 
