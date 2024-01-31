7f2b9b613669b88bb4a939ecfe24e473ccd26b89
Scott Hitchcock <scotthitch5@gmail.com>
2024-01-31 16:33:14 +1300

    feat: user redir to right route if  book id right
    as long as the user inputs the correct book id in trailing section of the route they will be redirected to the correct route which includes the book title and id

--- client/src/views/BookView.vue
+++ client/src/views/BookView.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import axios from 'axios'    
-import {useRoute} from "vue-router";
+import { useRoute } from "vue-router";
 import { ref, onMounted } from 'vue'
-
+import router from '../router'
 
 interface Book {
     id: number,
@@ -11,30 +11,38 @@ interface Book {
     price: number
 }
 
-const bookData = ref<Book[]>([]);
+// TODO: hmm need to figure out how to deal with this error
+const bookData = ref<Book>({});
 
+const bookTitleURLFormatter = (str: String): String => {
+  return str.toLowerCase().replace(/ /g, "-")
+}
 
 onMounted(() => {
     const route = useRoute()
 
-    console.log(route.params)
-
     axios.get(`books/${route.params.id}`)
     .then((res) => {
         bookData.value = res.data.book
+
+        // Always redirect to path with correct title format.
+        /* TODO: figure out if this always needs to be done or only if user inputs wrong 
+        book title */
+        router.push(`/${bookTitleURLFormatter(bookData.value.title)}-${bookData.value?.id}`)
     })
     .catch((err) => {
         console.error(err)
     })
+
 })
 
 </script>
 
 
 <template>
-<div>
-  {{ bookData }}
-</div>
+  <div>
+    {{ bookData }}
+  </div>
 </template>
 
 <style scoped>
