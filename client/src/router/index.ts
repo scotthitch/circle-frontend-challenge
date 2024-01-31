0651d636947c4964a9ebbafc4d9d13bd838818e7
Scott Hitchcock <scotthitch5@gmail.com>
2024-01-31 16:30:49 +1300

    feat: add redirect to bookview page
    the route is made from two parts:
    title: this-is-my-title
    id:5
    the id will be used to query the books api and get that individual book. The book data is then displayed in json format

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -10,8 +10,9 @@ const router = createRouter({
       component: HomeView
     },
     {
+      // Regex for id to access trailing digits
       path: '/:title-:id(\\d+)',
-      name: '/:title-:id(\\d+)',
+      name: 'bookview',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
