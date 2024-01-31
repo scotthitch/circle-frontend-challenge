9c65d9a75c433df52ac6c6a7ad73e24a8553fd8d
Scott Hitchcock <scotthitch5@gmail.com>
2024-01-31 16:00:47 +1300

    feat: add redirect to bookview page
    the route is made from two parts:
    title: this-is-my-title
    id:5
    the id will be used to query the books api and get that individual book. The book data is then displayed in json format

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -10,8 +10,8 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/book-view',
-      name: 'book-view',
+      path: '/:title-:id(\\d+)',
+      name: '/:title-:id(\\d+)',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
--- client/src/views/BookView.vue
+++ client/src/views/BookView.vue
@@ -1,10 +1,40 @@
 <script setup lang="ts">
+import axios from 'axios'    
+import {useRoute} from "vue-router";
+import { ref, onMounted } from 'vue'
+
+
+interface Book {
+    id: number,
+    title: string,
+    author: string,
+    price: number
+}
+
+const bookData = ref<Book[]>([]);
+
+
+onMounted(() => {
+    const route = useRoute()
+
+    console.log(route.params)
+
+    axios.get(`books/${route.params.id}`)
+    .then((res) => {
+        bookData.value = res.data.book
+    })
+    .catch((err) => {
+        console.error(err)
+    })
+})
 
 </script>
 
 
 <template>
-<div></div>
+<div>
+  {{ bookData }}
+</div>
 </template>
 
 <style scoped>
