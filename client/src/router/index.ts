ff6728aa9fac08dac0d60799c97d757ae55e00d7
Scott Hitchcock <scotthitch5@gmail.com>
2024-02-01 16:20:32 +1300

    chore: remove lazy loading

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import BookView from '../views/BookView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,10 +14,7 @@ const router = createRouter({
       // Regex for id to access trailing digits
       path: '/:title-:id(\\d+)',
       name: 'bookview',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/BookView.vue')
+      component: BookView
     }
   ]
 })
