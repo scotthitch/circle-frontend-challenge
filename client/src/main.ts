6ed5a918f1e49f33b111e71d8393c4782989a9e4
Scott Hitchcock <scotthitch5@gmail.com>
2024-02-01 17:06:04 +1300

    feat: add vuetify with dummy test

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,14 +1,23 @@
+import App from './App.vue'
+import { createApp } from 'vue'
+import axios from "axios";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import router from './router'
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import axios from "axios";
-
 axios.defaults.baseURL = "http://localhost:8000/";
+
+const vuetify = createVuetify({
+    components,
+    directives,
+  });
 
 const app = createApp(App)
 
 app.use(router)
+
+app.use(vuetify)
 
 app.mount('#app')
