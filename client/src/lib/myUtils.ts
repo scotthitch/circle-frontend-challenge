4daaf01975a25f829017baab14f56b5d27dc01d6
Scott Hitchcock <scotthitch5@gmail.com>
2024-02-01 16:25:33 +1300

    chore: change from String to string type

--- client/src/lib/myUtils.ts
+++ client/src/lib/myUtils.ts
@@ -1,9 +1,7 @@
-import type { RouteLocationRaw } from "vue-router"
-
-const spacesToKebabCase = (str: String): RouteLocationRaw => {
+const spacesToKebabCase = (str: string): string => {
   return str.toLowerCase().replace(/ /g, "-")
   }
 
-export const URLFormatter = (bookTitle: String, bookID: Number): RouteLocationRaw => {
-  return "/" + spacesToKebabCase(bookTitle) + "-" + bookID.toString();
+export const URLFormatter = (bookTitle: string, bookID: Number): string => {
+  return spacesToKebabCase(bookTitle) + "-" + bookID.toString();
 }
